4047fc9224ce041a4de2da5ee3f675ac9d374706
ARE VISHNU <137161020+vishnu-are@users.noreply.github.com>
2025-09-01 14:16:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+// Navbar toggle for mobile view
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+  window.scrollY > 50
+    ? (navbar.style.backgroundColor = "rgba(10, 10, 10, 0.98)")
+    : (navbar.style.backgroundColor = "rgba(10, 10, 10, 9.5)");
+});
